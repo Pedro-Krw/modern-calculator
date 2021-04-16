168d64382b5c77eeed3257c8c6e90f1674a8cc4f
pedrokorwa <korwapedro@gmail.com>
2021-04-16 23:41:07 +0700

    modern design calculator

---
+++ app/src/main/java/com/pedro/moderncalculator/MainActivity.java
@@ -0,0 +1,20 @@
+package com.pedro.moderncalculator;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
